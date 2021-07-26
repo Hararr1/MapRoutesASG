39c273f31a697eaba26cd7f7b9dd5c29821f9d35
kle1n.luk4 <kle1n.luk4@gmail.com>
2021-07-26 21:22:47 +0200

    Rename application to RoutesSPA

--- MapRoutes/e2e/src/app.e2e-spec.ts
+++ MapRoutes/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('MapRoutes app is running!');
+    expect(await page.getTitleText()).toEqual('RoutesSPA app is running!');
   });
 
   afterEach(async () => {
--- MapRoutes/src/app/main-Module/app.component.spec.ts
+++ MapRoutes/src/app/main-Module/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MapRoutes'`, () => {
+  it(`should have as title 'RoutesSPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MapRoutes');
+    expect(app.title).toEqual('RoutesSPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MapRoutes app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('RoutesSPA app is running!');
   });
 });
